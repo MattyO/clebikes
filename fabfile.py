1a0ef71738ddfcb8a8d71b4e7d9c5e812aac3c63
Matt ODonnell <matt.odonnell@leandog.com>
2013-05-28 01:01:26 -0400

    fab file added

---
+++ fabfile.py
@@ -0,0 +1,19 @@
+from fabric.api import run, env, cd
+from fabric.operations import put
+
+env.hosts = ['clebikes.com']
+env.forward_agent = True
+
+def test():
+    run("uname -s")
+
+def upgrade():
+    
+    with cd("/home/matt/srv/clebikes/"):
+        run("git pull")
+        put("clebikes/prod-settings.py", "clebikes/settings.py")
+        run("killall gunicorn")
+        run("source ../env/bin/activate && gunicorn clebikes.wsgi:application -D -w 4 --log-file gunicorn.log")
+        run("uname -s")
+
+
