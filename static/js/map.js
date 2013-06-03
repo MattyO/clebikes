81b8f936977633308e6babdec972cb831500c4ab
Matt ODonnell <matt.odonnell@leandog.com>
2013-06-03 00:33:36 -0400

    add labels to list.  ajaxed list

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,7 @@
+function indexToChar(anumber){
+    return String.fromCharCode(65 + parseInt(anumber));
+}
+
 function load_map() {
     var mapOptions = {
     center: new google.maps.LatLng(41.486132, -81.705087),
@@ -12,10 +16,10 @@ function add_points_to_map(points){
             for(key in points ){
                 var a_point = points[key];
                 points_array.push(new google.maps.Marker({
-                    icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|00FF00",
+                    icon: "http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld="+indexToChar(key)+"|00FF00|000000",
                     position: new google.maps.LatLng( a_point.location.latitude, a_point.location.longitude), 
                     map:map, 
-                    title:a_point.name
+                    title:a_point.name,
                 }));
             }
 }
@@ -26,6 +30,23 @@ function remove_points_from_map(){
     }
     points_array = Array();
 }
+function remove_points_from_list(){
+    $(".poi-table tbody").empty();
+}
+
+
+function add_points_to_list( points ){
+    var bodyElement = $(".poi-table tbody");
+    var currentRow = null;
+    $.each(points, function(index, value){
+        currentRow = bodyElement.append("<tr> </tr>");
+        currentRow.append("<td>" + indexToChar(index) + "</td>");
+        currentRow.append("<td>" + value.type.name + "</td>");
+        currentRow.append("<td>" + value.name + "</td>");
+        currentRow.append("<td>" + value.description + "</td>");
+    });
+}
+
 
 function get_points(args={}){
     $.ajax({
