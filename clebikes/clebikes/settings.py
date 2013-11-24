6aaf21268e938f5a95e2d548102f15ad3f70bcc9
Matt ODonnell <matt.odonnell@leandog.com>
2013-11-24 01:25:44 -0500

    static root config

--- clebikes/clebikes/settings.py
+++ clebikes/clebikes/settings.py
@@ -66,7 +66,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/srv/clebikes/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -79,7 +79,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-print STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
