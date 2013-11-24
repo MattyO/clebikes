b1eba6c58f2a659b1dd78979598ae4fbdabf6e85
Matt ODonnell <matt.odonnell@leandog.com>
2013-11-24 16:26:40 -0500

    stuff for seeding database with types and neighborhoods

--- clebikes/bikefinder/admin.py
+++ clebikes/bikefinder/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from bikefinder.models import POIType, POI, Neighborhood
+from bikefinder.models import POIType, POI, Neighborhood, Location
 
 admin.site.register(POIType)
 admin.site.register(POI)
 admin.site.register(Neighborhood)
-
+admin.site.register(Location)
